96bb9c722c0369799118e3fb3c7f2ebc179ed620
Giacomo <giacomo.marcon@barsanti.edu.it>
2020-04-19 12:42:07 +0200

    AGGIUNTO CLASSE COMPONENTE

---
+++ gestione-materiali/Componente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace distinta_base
+{
+    /// <summary>
+    /// Classe utilizzata per creare un nodo (radice inclusa)
+    /// </summary>
+
+    [Serializable]
+    public class Componente
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Codice { get; set; }
+        public int LeadTime { get; set; }
+        public int LeadTimeSicurezza { get; set; }
+        public int ScortaSicurezza { get; set; }
+        public int Lotto { get; set; }
+        public int PeriodoDiCopertura { get; set; }
+        public List<Componente> SottoNodi = new List<Componente>();
+    }
+
+
+}
